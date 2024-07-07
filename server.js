8a5e11affc90a46047390d7e4001b6e384453c9e
Harsh <harshsingh94070@gmail.com>
2024-07-07 21:50:55 +0530

    Hello

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app=express();
 const bodyParser=require('body-parser');
 const subroutes=require('./Routes/subroutes')
 app.use(bodyParser.json());
-
-
+require('dotenv').config();
+const PORT=process.env.PORT || 3000;
 app.get('/',(req,res)=>{
    res.send("Welcome to Subject_INFO");
 });
@@ -15,6 +15,6 @@ app.use('/data',subroutes);
 
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Listening on port 3000");
 })
